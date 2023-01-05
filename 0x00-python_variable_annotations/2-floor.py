229d5de34e4f897ed35cf8783cbdc1b930dc5a74
dainahwanjiku <deeshish2017@gmail.com>
2023-01-05 20:32:10 +0000

    floor

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -4,6 +4,6 @@ function on how to work out floor
 """
 
 
-def floor(n: float) ->: float
+def floor(n: float) -> float:
     """this is the floor work"""
-    return float(n)
+    return float(floor)
