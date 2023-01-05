09e94b76225c4428d8abd97ed69e50d7c7c1f6e9
dainahwanjiku <deeshish2017@gmail.com>
2023-01-05 13:59:21 +0000

    concat

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
 # function concat takes a string as args and returns concat string.
-def concat(str1 : string, str2 : string) -> string:
+def concat(str1 : str, str2 : str) -> str:
     #concation of two strings
     return str1 + str2
