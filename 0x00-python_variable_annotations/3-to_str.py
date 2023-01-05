ba29cf6f244929f801a5776e8c4ff99dcda23092
dainahwanjiku <deeshish2017@gmail.com>
2023-01-05 19:17:14 +0000

    to str

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+function takes a float as arg returns str rep of the float.
+"""
+
+
+def to_str(n: float) -> str:
+    """return string rep of the float"""
+    return str(n)
