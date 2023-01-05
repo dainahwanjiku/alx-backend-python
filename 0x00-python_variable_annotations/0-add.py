09bda7c1d8b4c264dce72e3c6c5f37e7001f7e75
dainahwanjiku <deeshish2017@gmail.com>
2023-01-05 13:36:46 +0000

    addition

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+# function add that takes a float as args and returns the sum.
+def add(a : float, b: float) -> float:
+    # addition of two variables of floats in datatype
+    return a + b
