aabe43146fbabbfdc2dcc4b69cee5f7665ff5750
dainahwanjiku <deeshish2017@gmail.com>
2023-01-05 19:41:26 +0000

    define var

---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+annotation of variables with specified values
+"""
+
+
+"""annotate variables with values"""
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
