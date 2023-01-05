afd12b593f9c911cdcdb7be40bb3b6f878a68187
dainahwanjiku <deeshish2017@gmail.com>
2023-01-05 21:38:55 +0000

    sum list

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 """
-function sum_list takes list input_list of floats as aargs returns sum as float.
+function takes list of floats as args returns sum as float.
 """
 from typing import List
 
@@ -9,4 +9,3 @@ from typing import List
 def sum_list(input_list: List[float]) -> float:
     """takes a list and returns sum as float"""
     return float(sum(input_list))
-
